6b32e31fed150cf5a36c0185d43555de2e8be1d6
yoonkh <zizou0812@gmail.com>
2017-06-01 20:10:36 +0900

    modify manage.py import

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
